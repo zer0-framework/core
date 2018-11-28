1ee40f408663a202df923adcc6bbea6e2181014d
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2018-11-28 22:40:41 +0700

    Cli

--- src/bootstrap-cli.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/bootstrap.php';
-require 'cli/vendor/autoload.php';
-
-if (!is_file(ZERO_ROOT . '/.env')) {
-    fwrite(STDERR, '.env file cannot be found. Make sure to run the script from the project directory.' . PHP_EOL);
-    exit(1);
-}
