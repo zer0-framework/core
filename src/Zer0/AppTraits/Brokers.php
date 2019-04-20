caeba9345f0e56b69c9ce172ab0c7d08ac35f2f2
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2019-04-21 04:50:33 +0800

    factory()

--- src/Zer0/AppTraits/Brokers.php
+++ src/Zer0/AppTraits/Brokers.php
@@ -17,36 +17,51 @@ trait Brokers
 
     /**
      * @alias broker()
-     * @param string $name
+     * @param string $nameOrClass
      * @return Base
      */
-    public function __invoke(string $name): Base
+    public function __invoke(string $nameOrClass): Base
     {
-        return $this->broker($name);
+        return $this->broker($nameOrClass);
     }
 
     /**
-     * @param string $name
+     * @param string $nameOrClass
+     * @param string $instance
+     * @param bool $caching
+     * @return mixed
+     */
+    public function factory(string $nameOrClass, string $instance = '', bool $caching = true)
+    {
+        return $this->broker($nameOrClass)->get($instance, $caching);
+    }
+
+    /**
+     * @param string $nameOrClass
      * @return Base
      */
-    public function broker(string $name): Base
+    public function broker(string $nameOrClass): Base
     {
-        $broker = $this->brokers[$name] ?? null;
+        $broker = $this->brokers[$nameOrClass] ?? null;
         if ($broker !== null) {
             return $broker;
         }
 
         try {
-            $class = $this->config->Brokers->getValue($name);
+            if (strpos($nameOrClass, '\\') === false) {
+                $class = $this->config->Brokers->getValue($nameOrClass);
+            } else {
+                $class = $nameOrClass;
+            }
             if ($class === null) {
                 $split = explode('\\', \Zer0\Brokers\Base::class);
-                $split[count($split) - 1] = $name;
+                $split[count($split) - 1] = $nameOrClass;
                 $class = implode('\\', $split);
             }
-            return $this->brokers[$name] = $broker = new $class($this);
+            return $this->brokers[$nameOrClass] = $broker = new $class($this);
         } finally {
             if (isset($broker)) {
-                $broker->setNameIfEmpty($name);
+                $broker->setNameIfEmpty($nameOrClass);
             }
         }
     }
