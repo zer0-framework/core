ce7fa1bd635bc8fa343147ecd1644e31d92f059c
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2020-05-13 18:42:29 +0300

    devtools dump -- phpdoc support

--- src/Zer0/Cli/Devtools.php
+++ src/Zer0/Cli/Devtools.php
@@ -6,6 +6,7 @@ use Zer0\Cli\AbstractController;
 
 /**
  * Class Build
+ *
  * @package Zer0\Cli\Controllers
  */
 final class Devtools extends AbstractController
@@ -18,7 +19,7 @@ final class Devtools extends AbstractController
     /**
      *
      */
-    public function phpStormMetaAction(): void
+    public function phpStormMetaAction (): void
     {
         $destfile = ZERO_ROOT . '/.phpstorm.meta.php';
 
@@ -28,7 +29,7 @@ final class Devtools extends AbstractController
 
         foreach ($autoload as $class => $path) {
             if (preg_match('~^Zer0\\\\Brokers\\\\(\\w+)~', $class, $match)) {
-                $name = $match[1];
+                $name  = $match[1];
                 $class = '\\' . $match[0];
                 if (!isset($brokers[$name]) && $name !== 'Base') {
                     $brokers[$name] = $class;
@@ -38,21 +39,35 @@ final class Devtools extends AbstractController
 
         $set = $override = $overrideFactory = '';
         foreach ($brokers as $name => $class) {
-            $name = var_export($name, true);
-            $set .= "        {$name},\n";
-            $override .= "        {$name} => {$class}::class,\n";
-            $refClass = new \ReflectionClass($class);
+
+            $eName       = var_export($name, true);
+            $set         .= "        {$eName},\n";
+            $override    .= "        {$eName} => {$class}::class,\n";
+            $refClass    = new \ReflectionClass($class);
             $returnClass = '\\' . (
-                $refClass->getMethod('get')->getReturnType()
-                ?? $refClass->getMethod('instantiate')->getReturnType()
-            );
+                    $refClass->getMethod('get')->getReturnType()
+                    ?? $refClass->getMethod('instantiate')->getReturnType()
+                );
+
+            $getReturn = function (string $method) use ($refClass): ?string {
+                $phpdoc = $refClass->getMethod('instantiate')->getDocComment();
+                if (preg_match('~@return (\S+)~', $phpdoc, $match)) {
+                    return $match[1];
+                }
+
+                return null;
+            };
+            if ($returnClass === '\\') {
+                $returnClass = $getReturn('get') ?? $getReturn('instantiate') ?? '\\';
+            }
+
             if ($returnClass === '\\') {
                 continue;
             }
-            $overrideFactory .= "        {$name} => {$returnClass}::class,\n";
+            $overrideFactory .= "        {$eName} => {$returnClass}::class,\n";
         }
-        $set = rtrim($set);
-        $override = rtrim($override);
+        $set             = rtrim($set);
+        $override        = rtrim($override);
         $overrideFactory = rtrim($overrideFactory);
 
         $date = date('r');
@@ -87,7 +102,7 @@ namespace PHPSTORM_META {
         $this->cli->successLine("Written to $destfile in " . $this->elapsedMill() . " ms.");
     }
 
-    public function dumpAction(): void
+    public function dumpAction (): void
     {
         $this->phpStormMetaAction();
     }
