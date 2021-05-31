0721f2f119bc1de0bcc575166e1835b95baf2b0d
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2021-05-31 12:54:42 +0300

    BaseException: previousString()

--- src/Zer0/Exceptions/BaseException.php
+++ src/Zer0/Exceptions/BaseException.php
@@ -4,15 +4,28 @@ namespace Zer0\Exceptions;
 
 /**
  * Class BaseException
+ *
  * @package Zer0\Exceptions
  */
 abstract class BaseException extends \Exception
 {
+    protected $previousStr;
+
     /**
      * @return array
      */
-    public function __sleep()
+    public function __sleep ()
     {
-        return ['message', 'code', 'file', 'line'];
+        $this->previousStr = (string)$this->getPrevious();
+
+        return ['message', 'code', 'file', 'line', 'previousString'];
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPreviousStr ()
+    {
+        return $this->previousStr ?? (string)$this->getPrevious ?? null;
     }
 }
